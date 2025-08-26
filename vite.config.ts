b61ddde63601ab63e6ce9dd21476c6219bbab4c6
Dmitrii Paskhin <d.pasxin@gmail.com>
2025-08-26 03:33:39 +0200

    fix: change base public path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '',
   root: './src/main',
   build: {
     outDir: '../../dist',
